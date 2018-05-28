601ba278b7458ceafaa1f25a8e16d69690f40c0e
Matt Mui <matt.mui15@gmail.com>
2018-05-28 00:05:28 -0400

    initial release

---
+++ reducers/index.js
@@ -0,0 +1,42 @@
+import { LOAD_REQUEST, LOAD_SUCCESS, LOAD_FAILURE } from '../actions';
+
+const initialState = {
+  isLoading: false,
+  isError: false,
+  isSuccess: false,
+  movies: []
+};
+
+export default function(state = initialState, action) {
+  switch(action.type) {
+    case LOAD_REQUEST: 
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+        isSuccess: false
+      };
+
+    case LOAD_SUCCESS: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+        isSuccess: true,
+        movies: action.movies
+      };
+    }
+
+    case LOAD_FAILURE: {
+      return {
+        ...state,
+        isLoading: true,
+        isError: true,
+        isSuccess: false
+      };
+    }
+
+    default:
+      return state;
+  }
+}
